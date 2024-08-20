570dbc4879192e469fa916a04837d6a3fbc7e90d
Soumi Saha <sahasoumi68@gmail.com>
2024-08-21 01:46:43 +0530

    add tabbed components in the bankist website

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -36,3 +36,39 @@ const section1 = document.querySelector('#section--1');
 btnScroll.addEventListener('click', () => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+//page navigation
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//tabbed components
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
